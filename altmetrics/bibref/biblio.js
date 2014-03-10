5a0ba3af00e0b3678743e27c183f279fed22f09c
Egon Willighagen <egon.willighagen@gmail.com>
2014-03-10 18:06:57 +0100

    Draft outline for database entry - paper link exposure

---
+++ altmetrics/bibref/biblio.js
@@ -0,0 +1,3 @@
+berjon.biblio = {
+   "Willighagen2013" : "Willighagen, E.;  Waagmeester, A.;  Spjuth, O.;  Ansell, P.;   Williams, A.;  Tkachenko, V.;  Hastings, J.;  Chen, B.;  Wild, D. Journal of Cheminformatics 2013, 5, 23, <a href=\"http://dx.doi.org/10.1186/1758-2946-5-23\">http://dx.doi.org/10.1186/1758-2946-5-23</a>.",
+}
