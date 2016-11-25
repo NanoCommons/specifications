044e4697bbfdfc86e25cc10dd6fcc79e3e1d4f8a
Egon Willighagen <egon.willighagen@gmail.com>
2016-11-25 16:48:34 +0100

    Initial JRC mappings

---
+++ jrc/bibref/biblio.js
@@ -0,0 +1,4 @@
+berjon.biblio = {
+   "Hastings2015" : "Hastings, J., Jeliazkova, N., Owen, G., Tsiliki, G., Munteanu, C. R., Steinbeck, C., Willighagen, E., Mar. 2015. eNanoMapper: harnessing ontologies to enable data integration for nanomaterial risk assessment. Journal of Biomedical Semantics 6 (1), 10+, <a href=\"http://dx.doi.org/10.1186/s13326-015-0005-5\">http://dx.doi.org/10.1186/s13326-015-0005-5</a>.",
+   "Noy2009" : "Noy, N. F., Shah, N. H., Whetzel, P. L., Dai, B., Dorf, M., Griffith, N., Jonquet, C., Rubin, D. L., Storey, M. A., Chute, C. G., Musen, M. A., May 2009. BioPortal: ontologies and integrated data resources at the click of a mouse. Nucleic Acids Research 37 (Web Server), W170-W173, <a href=\"http://dx.doi.org/10.1093/nar/gkp440\">http://dx.doi.org/10.1093/nar/gkp440</a>."
+}
