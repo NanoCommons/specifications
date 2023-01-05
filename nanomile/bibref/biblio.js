2942df71a5540a20639f9fef26bb48a3afc2ad47
Egon Willighagen <egon.willighagen@gmail.com>
2023-01-05 09:09:12 +0100

    First set of NanoMILE identifiers

---
+++ nanomile/bibref/biblio.js
@@ -0,0 +1,3 @@
+berjon.biblio = {
+   "VanRijn2022" : "Van Rijn, J., Afantitis A, Culha M, Dusinska M, Exner TE, Jeliazkova N, Longhin, E.M., Lynch, I., Melagraki, G., Nymark, P., Papadiamantis, A.G., Winkler, D.A., Yilmaz, H., Willighagen, E. August 2022. European Registry of Materials: global, unique identifiers for (undisclosed) nanomaterials. J Cheminform 14(1), 57, <a href=\"https://doi.org/10.1186/s13321-022-00614-7\">10.1186/s13321-022-00614-7</a>."
+}
